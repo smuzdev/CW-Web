e2c0ed9773e806a6ed39f309df8ab004d573ef69
smuzdev <smuzdev@github.com>
2019-11-28 01:37:40 +0300

    TY page fix

--- telegram.php
+++ telegram.php
@@ -27,7 +27,7 @@ foreach($arr as $key => $value) {
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
-  header('Location: menu.html');
+  header('Location: thank_you.html');
 } else {
   echo "Error";
 }
